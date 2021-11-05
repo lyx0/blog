0733eb14bb0f42e458eab2722b5402d0132ea730
lyx0 <66651385+lyx0@users.noreply.github.com>
2021-11-05 16:55:50 +0100

    add article handler

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -15,11 +16,14 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	l := log.New(os.Stdout, "blog", log.LstdFlags)
+
+	ah := handlers.NewArticle(l)
 
 	r.HandleFunc("/", handlers.HomeHandler)
-
+	r.Handle("/articles/{id:[0-9]+}", ah)
 	s := &http.Server{
-		Addr:         ":9090",
+		Addr:         ":8080",
 		Handler:      r,
 		IdleTimeout:  120 * time.Second,
 		WriteTimeout: 15 * time.Second,
@@ -27,6 +31,7 @@ func main() {
 	}
 
 	go func() {
+		logrus.Info("Listening on 8080")
 		err := s.ListenAndServe()
 		if err != nil {
 			logrus.Fatal(err)
